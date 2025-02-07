f325a0ac85033be66230369abed9a39c3bfcb69e
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-08 00:19:55 +0530

    add changes related to show method

--- MethodOverLoad.java
+++ MethodOverLoad.java
@@ -1,10 +1,10 @@
 class Test
 {
-    void show()
+    void show() //0 argument
     {
         System.out.println("called show method");
     }
-    void show()
+    void show(int a) // 1 argument
     {
         System.out.println("called 2nd show ");
     }
@@ -14,7 +14,9 @@ class Test
 public class MethodOverLoad {
     public static void main(String[] args) {
         Test t=new Test();
-        t.show();
-        
+       // t.show(); // call the 0 argument show method.
+
+       t.show(12); // call the 1 parameter show method.
+
     }
 }
